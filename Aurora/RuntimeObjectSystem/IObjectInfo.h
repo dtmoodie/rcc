9325757dc288f194cb5be176a012897be1ddba2e
dan <dtmoodie@gmail.com>
2017-01-05 11:33:46 -0500

    Should be unsigned int out

--- Aurora/RuntimeObjectSystem/IObjectInfo.h
+++ Aurora/RuntimeObjectSystem/IObjectInfo.h
@@ -2,7 +2,7 @@
 #include <string>
 struct IObjectInfo
 {
-    virtual int GetInterfaceId() const = 0;
+    virtual unsigned int GetInterfaceId() const = 0;
     // This is what actually gets displayed
     virtual std::string GetObjectName() const = 0;
     virtual std::string GetObjectTooltip() const = 0;
