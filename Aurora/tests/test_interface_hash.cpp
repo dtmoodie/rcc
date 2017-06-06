40e4fef71fa7a6fc75cc48f37837e9693a72aa9c
dan <dtmoodie@gmail.com>
2017-06-05 22:00:40 -0400

    fix for __PRETTY_FUNCTION__ not defined on windows.

--- Aurora/tests/test_interface_hash.cpp
+++ Aurora/tests/test_interface_hash.cpp
@@ -1,7 +1,10 @@
 #include "IObject.h"
 #include <iostream>
-
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCTION__
+#endif
 struct A{
+
     static constexpr uint32_t getHash() { return ct::hashClassName(__PRETTY_FUNCTION__); }
     static constexpr size_t classIdx(){ return ct::classNameIdx(__PRETTY_FUNCTION__); }
     static const char* name(){return __PRETTY_FUNCTION__;}
